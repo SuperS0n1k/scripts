3ca30627c3591ea4b9467694b9bb645da8c2c7cc
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-21 18:53:15 +0100

    Update and rename rootcon.js to rootco.js

--- rootco.js
+++ rootco.js
@@ -1,6 +1,6 @@
 
 document.querySelector('#app')._reactRootContainer._internalRoot.current.child.stateNode.props.store.getState().permissions.educator = true;
-document.querySelector('#app')._reactRootContainer._internalRoot.current.child.stateNode.props.store.getState().permissions.admin = false;
+document.querySelector('#app')._reactRootContainer._internalRoot.current.child.stateNode.props.store.getState().permissions.admin = true;
 document.querySelector('#app')._reactRootContainer._internalRoot.current.child.stateNode.props.store.getState().permissions.scratcher = true;
 document.querySelector('#app')._reactRootContainer._internalRoot.current.child.stateNode.props.store.getState().permissions.social = true;
 document.querySelector('#app')._reactRootContainer._internalRoot.current.child.stateNode.props.store.getState().permissions.educator_invitee = true;
