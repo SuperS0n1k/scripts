77fabbd051f11c706fa8a80f37d36538b7dc7459
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-08-21 16:39:00 +0100

    Update and rename rootcon.js to rootco.js

--- rootco.js
+++ rootco.js
@@ -6,6 +6,8 @@ document.querySelector('#app')._reactRootContainer._internalRoot.current.child.s
 document.querySelector('#app')._reactRootContainer._internalRoot.current.child.stateNode.props.store.getState().permissions.educator_invitee = true;
 let perms = document.querySelector('#app')._reactRootContainer._internalRoot.current.child.stateNode.props.store.getState().permissions;
 console.log(perms);
+console.log("installing")
 fetch('https://raw.githubusercontent.com/LoganAbel/ScratchMath/main/Math.js').then(r=>r.text()).then(t=>eval(t))
+console.log("done")
 fetch('https://raw.githubusercontent.com/SuperS0n1k/scratch3-extensions/main/ScratchJSnomods.js').then(r=>r.text()).then(t=>eval(t))
-
+console.log("done")
